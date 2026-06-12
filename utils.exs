d6bc3b72e48a8615941f7e152fce7a6294ee87f1
Magnus Larsen <magnushlarsen01@protonmail.com>
2026-06-12 13:14:58 +0200

    move print_usage to utils

--- utils.exs
+++ utils.exs
@@ -10,4 +10,24 @@ defmodule Utils do
       exit :shutdown
     end
   end
+
+  @default_usage_msg "Usage: elixir elil.exs [input_file]"
+
+  def print_usage(message) when is_binary(message), do: print_usage [message]
+
+  def print_usage(message) when is_list(message) do
+    List.pop_at(message, 0)
+      |> print_usage()
+  end
+
+  def print_usage({message, _rest})
+    when is_list(message) and length(message) == 0
+    when is_nil(message) do
+    IO.puts @default_usage_msg
+  end
+
+  def print_usage({message, rest}) when is_binary(message) do
+    IO.puts message
+    print_usage rest
+  end
 end
